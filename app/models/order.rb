204ef77dfb8c1dce36c7db9cc00a172a81d2790e
Donald Wu <yeukfei02@gmail.com>
2021-04-23 15:04:35 +0800

    feat: add validation in model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,8 @@
 class Order < ApplicationRecord
-    # has_many :users, :shops
+    has_many :users
+    has_many :shops
+
+    validates :order_detail, presence: true, allow_blank: false, allow_nil: false
+    validates :users_id, presence: true, allow_blank: false, allow_nil: false, numericality: { only_integer: true }
+    validates :shops_id, presence: true, allow_blank: false, allow_nil: false, numericality: { only_integer: true }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
     has_many :orders
+
+    validates :email, presence: true, allow_blank: false, allow_nil: false
+    validates :password, presence: true, allow_blank: false, allow_nil: false
 end
