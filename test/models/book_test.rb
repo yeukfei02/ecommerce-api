90c217b6e2719cbc090cb6eb08334c758ce25fc1
Donald Wu <yeukfei02@gmail.com>
2021-10-12 22:33:53 +0800

    feat: add annotate lib

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: books
+#
+#  id         :bigint           not null, primary key
+#  author     :string
+#  name       :string
+#  price      :float
+#  quantity   :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  shops_id   :bigint
+#
+# Indexes
+#
+#  index_books_on_shops_id  (shops_id)
+#
 require "test_helper"
 
 class BookTest < ActiveSupport::TestCase
